cc6a3cbd5a5a1a6497ed30bd388eb31387129cbc
Shivam020202 <charlieshivam70@gmail.com>
2026-04-10 14:39:41 +0530

    fixes

--- includes/header-links.php
+++ includes/header-links.php
@@ -1,14 +1,24 @@
- <!-- Google Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&display=swap"
-        rel="stylesheet">
+<!-- Google Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 
-    <link rel="icon" href="https://newoaks.s3.us-west-1.amazonaws.com/AutoDev/17785/a4b563f6-14e0-47a9-896c-efe62b3be293.webp">
+<link rel="icon"
+    href="https://newoaks.s3.us-west-1.amazonaws.com/AutoDev/17785/a4b563f6-14e0-47a9-896c-efe62b3be293.webp">
 
-    <!-- Lucide Icons -->
-    <script src="https://unpkg.com/lucide@latest"></script>
+<!-- Lucide Icons -->
+<script src="https://unpkg.com/lucide@latest"></script>
 
-    <!-- Stylesheets -->
-    <link rel="stylesheet" href="style.css">
-    <script src="main4.js"></script>
+<!-- Stylesheets -->
+<link rel="stylesheet" href="style.css">
+<script src="main4.js"></script>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11034311341">
+</script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'AW-11034311341');
+</script>
