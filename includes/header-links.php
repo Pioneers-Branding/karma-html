e9da74ccdd607aa28cfb48b94275eee94a9cf129
Shivam020202 <charlieshivam70@gmail.com>
2026-04-30 14:42:58 +0530

    website fixe

--- includes/header-links.php
+++ includes/header-links.php
@@ -22,3 +22,13 @@
 
     gtag('config', 'AW-11034311341');
 </script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-11034311341"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11034311341');
+</script>
