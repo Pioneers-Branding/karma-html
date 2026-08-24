2b8a4b4e407f2e3edb8492299353b151803d5393
Pioneers-Branding <bpwithlinux@gmail.com>
2026-08-24 16:18:10 +0530

    inquire lp changes

--- includes/header-links.php
+++ includes/header-links.php
@@ -3,8 +3,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
 
-<link rel="icon"
-  href="/assets/images/img-f252510da8d5.webp">
+<link rel="icon" href="/assets/images/img-f252510da8d5.webp">
 
 <!-- Lucide Icons -->
 <script src="https://unpkg.com/lucide@latest"></script>
@@ -21,21 +20,23 @@
 
   gtag('config', 'AW-11034311341');
 </script>
-<meta name="facebook-domain-verification" content="bz0g3gv4zo8vcf9q4dtb2baafw075q" />
+
 <!-- Meta Pixel Code -->
 <script>
-!function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '1943390473007545');
-fbq('track', 'PageView');
+  !function (f, b, e, v, n, t, s) {
+    if (f.fbq) return; n = f.fbq = function () {
+      n.callMethod ?
+      n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+    };
+    if (!f._fbq) f._fbq = n; n.push = n; n.loaded = !0; n.version = '2.0';
+    n.queue = []; t = b.createElement(e); t.async = !0;
+    t.src = v; s = b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t, s)
+  }(window, document, 'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '1943390473007545');
+  fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1943390473007545&ev=PageView&noscript=1"
-/></noscript>
+    src="https://www.facebook.com/tr?id=1943390473007545&ev=PageView&noscript=1" /></noscript>
 <!-- End Meta Pixel Code -->
